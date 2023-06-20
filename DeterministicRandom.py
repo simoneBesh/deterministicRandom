f9c7be89c2bbccf2b47ccc48f2cc3733848a0251
simoneBesh <70675253+simoneBesh@users.noreply.github.com>
2023-06-20 14:59:41 -0400

    Add files via upload

---
+++ DeterministicRandom.py
@@ -0,0 +1,12 @@
+import random
+import math
+
+random.seed(72)
+
+sequence1 = [random.random() for i in range(10)]
+print ("sequence 1: ", sequence1)
+
+random.seed(72)
+
+sequence2 = [random.random() for i in range (10)]
+print ("sequence 2: ", sequence2)
